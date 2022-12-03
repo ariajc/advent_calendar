897c71d78d956179dd36c65b8c7da6acfa6a29c2
ariajc <ajaimeco73@alumnes.ub.edu>
2022-12-03 11:00:41 +0100

    update

---
+++ 3/code.py
@@ -0,0 +1,35 @@
+import string
+import os
+
+
+os.chdir('C:/Users/AriaJaimejuan/OneDrive - Mentice AB/Desktop/advent/advent_calendar/3/')
+
+values = dict()
+for index , letter in enumerate(string.ascii_lowercase+string.ascii_uppercase):
+    values[letter]=index+1
+
+with open('data.txt') as f:
+    lines=f.readlines()
+
+sum=0
+
+for line in lines:
+    s_1=line[slice(len(line)//2)]
+    s_2=line[slice(len(line)//2,len(line))]
+    sum+=values[list(set(s_1).intersection(set(s_2)))[0]]
+            
+print(sum)
+
+
+
+## p2
+sum_2=0
+
+for i in range(0,len(lines),3):
+    sum_2+=values[list(set(lines[i].strip()).intersection(set(lines[i+1].strip()),set(lines[i+2].strip())))[0]]
+
+print(sum_2)
+
+
+
+
